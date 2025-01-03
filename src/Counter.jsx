c0a001a109dd32c4fd34a87759306ea36fce9ef4
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-03 21:14:34 +0530

    the props object is stored into a constant by defining its attributes and passed the props only in the function passing

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Counter(props) {
+const {title,count} = props
   return (
     <div>
-      <h2>{props.title} {props.count}</h2>
+      <h2>{title} {count}</h2>
     </div>
   )
 }
