97f1d4bfe49dd503cf5219ad54573323e402726c
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-03 13:31:11 +0530

    Counter completed using useState hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-function App() {
-  return (
+import { useState } from "react"
 
+function App() {
+  const [count, setCount] = useState(0)
+  const Increment=()=> {
+    setCount(count+1)
+  }
+  const Decrement=()=> {
+    setCount(count-1)
+  }
+  return (
+    <div>
+      <h2>Count : {count}</h2>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+    </div>
   );
 }
 
